fc22b83b9aac5d8821b67011e15a6172dbbd13e0
sunny <indiasuny000@gmail.com>
2015-01-25 20:55:32 +0530

    Clean listing with music player and bin command added.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,14 @@
 
 var galleryApp = angular.module('galleryApp', []);
 
-galleryApp.controller('GalleryListCtrl', function($scope, $http) {
+galleryApp.controller('GalleryListCtrl', function($scope, $http, $document) {
   $http.get('/music')
   .success(function(data, status, headers, config) {
     console.log('type: ', typeof(data));
     $scope.items = data;
   });
+
+  $scope.songSelect = function (songPath) {
+    $scope.selectedSongPath = songPath;
+  }
 });
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
-var express = require('express'),
-    fs = require('fs')
-    Q  = require('q'),
-    readdirrsync = require('readdirrsync');
+var express      = require('express'),
+    fs           = require('fs')
+    Q            = require('q'),
+    readdirrsync = require('readdirrsync'),
+    _            = require('lodash'),
+    path         = require('path');
 
 var app = express();
 
@@ -12,43 +14,31 @@ app.set('json spaces', 2);
 app.use('/app', express.static(__dirname + '/app'));
 app.use('/explore', express.static(process.argv[2]));
 
-/*
-app.get('/gallery', function(req, res) {
-  var files = fs.readdirSync('/Users/sunny/Desktop/rec/recup/');
-  console.log('file count: ', files.length);
-  files.forEach(function (file, index) {
-    files[index] = '/Users/sunny/Desktop/rec/recup/' + file;
-  });
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.end(JSON.stringify(files));
-});
-*/
-
 app.get('/music', function (req, res) {
   var files = readdirrsync(process.argv[2]);
-  console.log('file count:', files.length);
-  console.log(files);
+  var ext;
+  filterByExtension(files, ['mp3']);
   var slicePos = process.argv[2].length;
   files.forEach(function (file, index) {
-    files[index] = '/explore/' + file.slice(slicePos);
+    files[index] = {
+      path: '/explore/' + file.slice(slicePos),
+      name: path.basename(file)
+    }
   });
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify(files));
 });
-
-console.log('Serving ' + process.argv[2]);
 
 var server = app.listen(3000, function() {
   console.log('Listening on port %d', server.address().port);
 });
 
-
-function readDirSync(path) {
-  return Q.Promise(function (resolve, reject) {
-    recursive(path, function (err, files) {
-      if (err) reject(err);
-      resolve(files);
-    });
-  });
+function filterByExtension(list, ext) {
+  list.forEach(function (item, index) {
+    if (! _.contains(ext, item.split('.').pop())) {
+      list.splice(index, 1);
+      filterByExtension(list, ext);
+      return;
+    }
+  })
 }
